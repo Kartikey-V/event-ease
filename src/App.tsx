89f18d2732524a1a546a0ec25e3b76528f3a9e95
Kartikey Vashisht <76244793+Kartikey-V@users.noreply.github.com>
2024-02-17 14:15:55 +0530

    added eslint, Added React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import React from 'react';
 import Header from './components/Header';
 import './App.css';
 import Body from './components/Body';
 
 function App() {
-  return (
-    <>
-   <Header />
-   <Body />
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Body />
+		</>
+	);
 }
 
 export default App;
