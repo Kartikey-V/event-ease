30b51596676ccb36296be0adaefb5c87c4cc355b
Kartikey Vashisht <76244793+Kartikey-V@users.noreply.github.com>
2024-02-17 13:20:32 +0530

    Added MUI, Added Home Page

---
+++ src/components/Body/constants.js
@@ -0,0 +1,16 @@
+export const data = [
+	{
+		name: 'Frozen yoghurt',
+		calories: 1,
+		fat: 2,
+		carbs: 3,
+		protein: 4,
+	},
+	{
+		name: 'Ice cream sandwich',
+		calories: 5,
+		fat: 6,
+		carbs: 7,
+		protein: 8,
+	},
+];
